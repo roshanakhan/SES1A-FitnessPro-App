3917d18777963fb3ef5ab1f6fb9971943a3b8391
Roshana Khan <roshana.khan@live.com>
2018-04-15 20:30:03 +1000

    BMI complete commit

--- app/src/main/java/com/example/roshanakhan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/roshanakhan/myapplication/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.roshanakhan.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText height, weight;
+    TextView result;
+    Button calculateBmi;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (TextView) findViewById(R.id.result);
+        calculateBmi = (Button) findViewById(R.id.calculateBmi);
+
+        calculateBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBMI();
+
+            }
+        });
+
+    }
+
+    private void calculateBMI(){
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr) && weightStr != null && !"".equals(weightStr) ) {
+            float heightValue = Float.parseFloat(heightStr) /100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+        }
+
+    }
+
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = "very severely underweight";
+        } else if (Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 16f) <= 0) {
+            bmiLabel = "severely underweight";
+        } else if (Float.compare(bmi, 16f) > 0 && Float.compare(bmi, 18.5f) <= 0) {
+            bmiLabel = "underweight";
+        } else if (Float.compare(bmi, 18.5f) > 0 && Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = "normal weight";
+        } else if (Float.compare(bmi, 25f) > 0 && Float.compare(bmi, 30f) <= 0) {
+            bmiLabel = "overweight";
+        } else if (Float.compare(bmi, 30f) > 0 && Float.compare(bmi, 35f) <= 0) {
+            bmiLabel = "obese";
+        } else if (Float.compare(bmi, 35f) > 0 && Float.compare(bmi, 40f) <= 0) {
+            bmiLabel = "very obese";
+        } else {
+            bmiLabel = "very severely obese";
+        }
+
+        bmiLabel = "BMI: " + bmi + "\n" + "Your are " + bmiLabel;
+        result.setText(bmiLabel);
     }
 }
